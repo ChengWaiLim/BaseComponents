f0d3c77ca54832e2e4995ca869339036e1fce732
Cheng Wai Lim(Home) <william@ibasezero.com>
2020-03-23 11:58:22 +0800

    add BottomNavigationBar

--- basecomponents/src/main/java/hk/com/chengwailim/basecomponents/Components/BottomNavigationBar.kt
+++ basecomponents/src/main/java/hk/com/chengwailim/basecomponents/Components/BottomNavigationBar.kt
@@ -2,7 +2,27 @@ package hk.com.chengwailim.basecomponents.Components
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.Menu
+import androidx.navigation.NavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-open class BottomNavigationBar @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = com.google.android.material.R.attr.bottomNavigationStyle) : BottomNavigationView(context, attrs, defStyleAttr) {
+
+class BottomNavigationBar@JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : BottomNavigationView(context, attrs, defStyleAttr){
+    fun init(controller: NavController, modelList: ArrayList<NavigationModel>){
+        for (i in 0 until modelList.size ){
+            addNavigationModel(i, modelList.get(i))
+        }
+        this.setOnNavigationItemSelectedListener {
+            controller.navigate(modelList.get(it.itemId).fragment_id)
+            true
+        }
+    }
+
+    private fun addNavigationModel(itemID:Int, model: NavigationModel){
+        this.getMenu().add(Menu.NONE, itemID, Menu.NONE, model.title).setIcon(model.icon);
+    }
+
+    data class NavigationModel(var title:String, var icon:Int, var fragment_id: Int)
 }
