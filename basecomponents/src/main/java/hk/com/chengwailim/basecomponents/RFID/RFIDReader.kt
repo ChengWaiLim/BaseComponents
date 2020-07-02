e40777c67ce1c584f4b49b19fb634f1f0102a64f
Cheng Wai Lim <s1113618@s.eduhk.hk>
2020-07-02 15:32:41 +0800

    Add locating tag event listener

--- basecomponents/src/main/java/hk/com/chengwailim/basecomponents/RFID/RFIDReader.kt
+++ basecomponents/src/main/java/hk/com/chengwailim/basecomponents/RFID/RFIDReader.kt
@@ -15,10 +15,11 @@ class RFIDReader {
         var rfidReader: RFIDReader? = null
         var readerDevice: ReaderDevice? = null
         var readTagEventsListener: RfidEventsListener? = null
+        var locateTagEventsListener: RfidEventsListener? = null
         init { readers = Readers() }
 
-        fun setUpReader(context: Activity, callback: (isConnected: Boolean) -> Unit = {}, onReadTagCallback: (tag: String) -> Unit = {}) {
-             object : AsyncTask<Void, Void, Exception?>() {
+        fun setUpReader(context: Activity, callback: (isConnected: Boolean) -> Unit = {}) {
+            object : AsyncTask<Void, Void, Exception?>() {
                 override fun onPreExecute() {
                     BaseDialog.showLoading(context)
                 }
@@ -54,15 +55,40 @@ class RFIDReader {
                         context.getString(R.string.reader_connected)
                     )
                     callback.invoke(isConnected())
-                    if(isConnected())
-                        this@Companion.setReadTagEvent(object : RFIDReadTagListener{
-                            override fun onTagRead(tagID: String) {
-                                onReadTagCallback(tagID)
-                            }
-                        })
                 }
             }.execute()
         }
+        public fun locateTag(tag:String, onTagRead: (distance: String) -> Unit){
+            if(isConnected()){
+                rfidReader?.Events?.setTagReadEvent(true)
+                val eventsListener = object : RfidEventsListener{
+                    override fun eventStatusNotify(p0: RfidStatusEvents?) {}
+                    override fun eventReadNotify(p0: RfidReadEvents?) {
+                        val scannedTags = rfidReader?.Actions?.getReadTags(100)
+                        if (scannedTags != null) {
+                            for (index in scannedTags.indices) {
+                                if(scannedTags[index].isContainsLocationInfo()){
+                                    onTagRead.invoke(scannedTags[index].LocationInfo.relativeDistance.toString())
+                                }
+                            }
+                        }
+                    }
+                }
+                locateTagEventsListener = eventsListener;
+                this.rfidReader?.Events?.addEventsListener(locateTagEventsListener)
+                this.rfidReader?.Actions?.TagLocationing?.Perform(tag, null, null);
+            }
+        }
+
+        public fun stopLocateTag(){
+            if(isConnected()){
+                rfidReader?.Actions?.Inventory?.stop()
+                rfidReader?.Events?.removeEventsListener(
+                    locateTagEventsListener
+                )
+            }
+        }
+
 
         private fun disconnect() {
             if (rfidReader != null && rfidReader!!.isConnected) rfidReader?.disconnect()
@@ -73,16 +99,23 @@ class RFIDReader {
             return rfidReader!!.isConnected
         }
 
-        fun setReadTagEvent(readTagEventsListener: RFIDReadTagListener) {
+        fun startReadTag(onTagRead: (tag:String) -> Unit){
             if(isConnected()){
                 rfidReader?.Events?.setTagReadEvent(true)
+                val readTagEventsListener = object : RfidEventsListener{
+                    override fun eventStatusNotify(p0: RfidStatusEvents?) {}
+                    override fun eventReadNotify(p0: RfidReadEvents?) {
+                        val tagDataArray: TagDataArray? = rfidReader?.Actions?.getReadTagsEx(100)
+                        if (tagDataArray != null) {
+                            for (i in 0 until tagDataArray.length) {
+                                var tagID = tagDataArray.tags[i].tagID
+                                onTagRead(tagID)
+                            }
+                        }
+                    }
+                }
                 Companion.readTagEventsListener = readTagEventsListener
                 rfidReader?.Events?.addEventsListener(readTagEventsListener)
-            }
-        }
-
-        fun startReadTag(){
-            if(isConnected()){
                 rfidReader?.Actions?.Inventory?.perform()
             }
         }
@@ -90,11 +123,6 @@ class RFIDReader {
         fun stopReadTag(){
             if(isConnected()){
                 rfidReader?.Actions?.Inventory?.stop()
-            }
-        }
-
-        fun removeReadTagEvent(){
-            if(isConnected()){
                 rfidReader?.Events?.removeEventsListener(
                     readTagEventsListener
                 )
